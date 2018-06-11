f9114013143f2804dc57a3d6bc444f448ee6da0a
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-11 13:35:18 +0300

    Add webpack with bootstrap

--- Makefile
+++ Makefile
@@ -5,7 +5,14 @@ publish:
 	npm publish
 
 lint:
-	npm run eslint
+	npm run eslint .
 
 test:
 	npm test
+
+build:
+	rm -rf dist
+	NODE_ENV=production npm run webpack
+
+develop:
+	npm run webpack-serve
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+};
