838a2f0bf456f098cc4c47d274b1b6b246c26d22
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-12 14:33:35 +0300

    Update validator with storage includes

--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,4 @@
 import isURL from 'validator/lib/isURL';
 
-export default string => isURL(string, { require_protocol: true });
+export default (string, storage) =>
+  isURL(string, { require_protocol: true }) && !storage.includes(string);
