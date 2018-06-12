8fcc3f4722873b382f182505ad5231060aa07ba7
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-12 18:28:00 +0300

    Remove entry and output points

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
