a93481bc76186aa928c6cc9597afe56a71f896e5
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-12 20:47:09 +0300

    Add empty string validation

--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,4 @@
 import isURL from 'validator/lib/isURL';
 
 export default (string, storage) =>
-  isURL(string, { require_protocol: true }) && !storage.includes(string);
+  (isURL(string, { require_protocol: true }) && !storage.includes(string)) || string === '';
