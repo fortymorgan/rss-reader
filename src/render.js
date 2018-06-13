84c09fda2cfb7b6b717158e32a723477be0e6263
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-13 14:31:40 +0300

    Add opening on new tab to links

--- src/render.js
+++ src/render.js
@@ -12,7 +12,7 @@ export default (feedsList) => {
     getRssData(feed).then((dataArray) => {
       const htmlListItems = dataArray.map(({ description, link, title }) => {
         const html = `<div>
-          <a href="${link}" class="list-group-item list-group-item-action flex-column align-items-start">
+          <a href="${link}" class="list-group-item list-group-item-action flex-column align-items-start" target="_blank">
             <div class="d-flex w-100 justify-content-between">
               <h5 class="mb-1">${title}</h5>
             </div>
