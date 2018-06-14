414f8ca883cb87355ab0fe37bd182fcb5c9081bb
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-14 22:46:51 +0300

    Change CORS proxy

--- src/getter.js
+++ src/getter.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import parseRssXml from './parser';
 
-const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+const corsProxy = 'https://cors-proxy.htmldriven.com/?url=';
 
 export default feed => axios.get(corsProxy + feed.url)
   .then(blob => blob.data)
   .then((data) => {
     const parser = new DOMParser();
-    const xml = parser.parseFromString(data, 'application/xml');
+    const xml = parser.parseFromString(data.body, 'application/xml');
     return parseRssXml(xml);
   });
