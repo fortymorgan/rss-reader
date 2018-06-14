7e4b1336026085d6cf8af26209ad697b36ec76e0
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-14 15:32:32 +0300

    Merge modal callback to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import $ from 'jquery';
 import updateState from './stateUpdater';
 import isInputValid from './validator';
-import modalCallback from './modal';
 import { getFeedsList } from './storage';
-import update from './feedsUpdater';
+import updateFeeds from './feedsUpdater';
+
+function modalCallback(event) {
+  const modal = $('#modal');
+  const button = $(event.relatedTarget);
+  const link = button.prev();
+  const title = link.children()[0].innerText;
+  const description = link.children()[1].innerText;
+  modal.find('.modal-title').text(title);
+  modal.find('.modal-body').text(description);
+}
 
 export default () => {
   const state = {
@@ -64,5 +73,5 @@ export default () => {
   $('#modal').on('show.bs.modal', modalCallback);
 
   updateState(state);
-  setTimeout(() => update(state), 5000);
+  setTimeout(() => updateFeeds(state), 5000);
 };
