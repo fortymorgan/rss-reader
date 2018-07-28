1b12f16c1785a93ce6d05381be02343fb87e146a
Forty Morgan <dimazhmudikov@gmail.com>
2018-07-28 16:04:51 +0300

    Rename reducers index

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import reducers from './reducers/reducers';
+import reducers from './reducers';
 import App from './components/App.jsx';
 import { getFeedsList } from './storage';
 import * as actions from './actions';
